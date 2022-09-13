c6a19faa6b38b42446f8b882eac1ab1aa05e2c9b
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-09-13 20:47:21 -0300

    📝 update(mobile): add components on home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,42 @@
-import React from 'react';
-
 import {
+  FlatList,
+  Image,
   View
 } from 'react-native';
 
+import logoImg from '../../assets/logo-nlw-esports.png';
+import { GameCard } from '../../components/GameCard';
+import { Heading } from '../../components/Heading';
+
 import { styles } from './styles';
+
+import { GAMES } from '../../utils/games';
 
 export function Home() {
   return (
-    <View>
+    <View style={styles.container}>
+      <Image
+        source={logoImg}
+        style={styles.logo}
+      />
 
+      <Heading
+        title="Encontre o seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <FlatList
+        contentContainerStyle={styles.contentList}
+        data={GAMES}
+        keyExtractor={item => item.id}
+        horizontal
+        renderItem={({ item }) => (
+          <GameCard
+            data={item}
+          />
+        )}
+        showsHorizontalScrollIndicator={false}
+      />
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,5 +3,18 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center'
+  },
+
+  logo: {
+    marginBottom: 48,
+    marginTop: 74,
+    height: 120,
+    width: 214,
+  },
+
+  contentList: {
+    paddingLeft: 32,
+    paddingRight: 64
   }
 });
