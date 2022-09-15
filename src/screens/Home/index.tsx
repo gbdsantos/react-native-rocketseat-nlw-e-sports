b7c2b6ee74ea8d0465ca5075400682abade2531f
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-09-15 13:45:21 -0300

    🔌 update(mobile): connect to the api and list the games

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,25 @@
+import { useEffect, useState } from 'react';
 import {
   FlatList,
   Image,
   View
 } from 'react-native';
 
-import logoImg from '../../assets/logo-nlw-esports.png';
-import { GameCard } from '../../components/GameCard';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
 
+import logoImg from '../../assets/logo-nlw-esports.png';
 import { styles } from './styles';
 
-import { GAMES } from '../../utils/games';
-
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect(() => {
+    fetch('http://192.168.1.19:3333/games')
+      .then(response => response.json())
+      .then(data => setGames(data));
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image
@@ -27,7 +34,7 @@ export function Home() {
 
       <FlatList
         contentContainerStyle={styles.contentList}
-        data={GAMES}
+        data={games}
         keyExtractor={item => item.id}
         horizontal
         renderItem={({ item }) => (
