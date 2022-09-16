41ea322afeab9c06fd105c82c329f51a140cac23
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-09-15 22:22:11 -0300

    🎨 styles(mobile): create game screen

---
+++ src/screens/Game/index.tsx
@@ -0,0 +1,18 @@
+import {
+  View
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { Background } from '../../components/Background';
+
+import { styles } from './styles';
+
+export function Game() {
+  return (
+    <Background>
+      <SafeAreaView>
+
+      </SafeAreaView>
+    </Background>
+  );
+}
